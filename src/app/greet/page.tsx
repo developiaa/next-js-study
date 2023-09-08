13a8841ea6584cd4ae684e84d6417b34aa9e6392
developiaa <developiaa@gmail.com>
2023-09-08 23:35:45 +0900

    feat: 기본 라우팅 추가

---
+++ src/app/greet/page.tsx
@@ -0,0 +1,6 @@
+function Greet2() {
+    return <div>This is the Greet2</div>
+}
+
+export default Greet2;
+
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,6 @@
+function HomePage2() {
+    return <div>This is the homepage22222</div>
+}
+
+export default HomePage2;
+
